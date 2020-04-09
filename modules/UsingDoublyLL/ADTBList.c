80d6f89fbd202fa08456f1e7c3b812520f1abe94
Kostas Chatzikokolakis <kostas@chatzi.org>
2020-04-09 18:03:21 +0300

    list cleanup

--- modules/UsingDoublyLL/ADTBList.c
+++ modules/UsingDoublyLL/ADTBList.c
@@ -1,6 +1,6 @@
 ///////////////////////////////////////////////////////////
 //
-// Υλοποίηση του ADT BList μέσω συνδεδεμένης λίστας.
+// Υλοποίηση του ADT BList μέσω διπλά συνδεδεμένης λίστας.
 //
 ///////////////////////////////////////////////////////////
 
@@ -28,40 +28,12 @@ int blist_size(BList blist) {
 	return 0;
 }
 
-BListNode blist_first(BList blist) {
-	return NULL;
-
-}
-
-BListNode blist_last(BList blist) {
-	return NULL;
-
-}
-
-BListNode blist_next(BList blist, BListNode node) {
-	return NULL;
-
-}
-
-BListNode blist_previous(BList blist, BListNode node) {
-	return NULL;
-
-}
-
-Pointer blist_node_value(BList blist, BListNode node) {
-	return NULL;
-}
-
 void blist_insert(BList blist, BListNode node, Pointer value) {
 
 }
 
 void blist_remove(BList blist, BListNode node) {
 
-}
-
-BListNode blist_find_node(BList blist, Pointer value, CompareFunc compare) {
-	return NULL;
 }
 
 Pointer blist_find(BList blist, Pointer value, CompareFunc compare) {
@@ -74,4 +46,31 @@ DestroyFunc blist_set_destroy_value(BList blist, DestroyFunc destroy_value) {
 
 void blist_destroy(BList blist) {
 
+}
+
+
+// Διάσχιση της λίστας /////////////////////////////////////////////
+
+BListNode blist_first(BList blist) {
+	return NULL;
+}
+
+BListNode blist_last(BList blist) {
+	return NULL;
+}
+
+BListNode blist_next(BList blist, BListNode node) {
+	return NULL;
+}
+
+BListNode blist_previous(BList blist, BListNode node) {
+	return NULL;
+}
+
+Pointer blist_node_value(BList blist, BListNode node) {
+	return NULL;
+}
+
+BListNode blist_find_node(BList blist, Pointer value, CompareFunc compare) {
+	return NULL;
 }
